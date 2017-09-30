3079d3d36c3d0710b6f829955959c5ca12e6f71b
abrarShariar <shariarabrar@gmail.com>
2017-09-30 14:23:06 +0600

    insertion sort

---
+++ sorting/insertion_sort.js
@@ -0,0 +1,23 @@
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+
+process.stdin.on('data',(data)=>{
+  let input_array = data.split(" ");
+  for(let i = 1;i<input_array.length;i++){
+    let key = input_array[i];
+    let j = i - 1;
+    while(j>=0 && input_array[j] > key){
+      input_array[j+1] = input_array[j];
+      j--;
+    }
+    input_array[j+1] = key;
+  }
+  console.log(input_array);
+  // process.stdout.write(input_array);
+});
+//
+// function showOutput(input_array){
+//   for(let i=0;i<input_array.length;i++){
+//     process.stdout.write(input_array[i]);
+//   }
+// }
